dbc0a0e3f15daa4abaa8a491fc5c907cb3d93ff7
Dean McGregor <deanm@synopsys.com>
2018-05-06 12:55:11 +1000

    Fix up imports

--- ynab/__init__.py
+++ ynab/__init__.py
@@ -16,15 +16,15 @@
 from __future__ import absolute_import
 
 # import apis into sdk package
-from ynab.accounts_api import AccountsApi
-from ynab.budgets_api import BudgetsApi
-from ynab.categories_api import CategoriesApi
-from ynab.months_api import MonthsApi
-from ynab.payee_locations_api import PayeeLocationsApi
-from ynab.payees_api import PayeesApi
-from ynab.scheduled_transactions_api import ScheduledTransactionsApi
-from ynab.transactions_api import TransactionsApi
-from ynab.user_api import UserApi
+from ynab.ynab.accounts_api import AccountsApi
+from ynab.ynab.budgets_api import BudgetsApi
+from ynab.ynab.categories_api import CategoriesApi
+from ynab.ynab.months_api import MonthsApi
+from ynab.ynab.payee_locations_api import PayeeLocationsApi
+from ynab.ynab.payees_api import PayeesApi
+from ynab.ynab.scheduled_transactions_api import ScheduledTransactionsApi
+from ynab.ynab.transactions_api import TransactionsApi
+from ynab.ynab.user_api import UserApi
 
 # import ApiClient
 from ynab.api_client import ApiClient
